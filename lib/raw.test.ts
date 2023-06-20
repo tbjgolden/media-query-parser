286598c5d6c5daf9da30115ea0fb7b38d076b35e
Tom Golden <oss@tom.bio>
2023-06-20 20:12:00 +0100

    test: added more tests

---
+++ lib/raw.test.ts
@@ -0,0 +1,47 @@
+import fs from "node:fs";
+import path from "node:path";
+import { isParserError, parseMediaQueryList } from "./index.js";
+
+test("parseMediaQueryList", () => {
+  const testQueryLists = JSON.parse(
+    fs.readFileSync(path.join(process.cwd(), "lib/__fixtures__/raw.json"), "utf8")
+  );
+  const errorsMap = JSON.parse(
+    fs.readFileSync(path.join(process.cwd(), "lib/__fixtures__/errors.json"), "utf8")
+  );
+
+  const shouldBeErrorSet = new Set([
+    `print" is not supported in IE6 or 7.  Fully patched IE8 should be OK */\n@media print`,
+    `statement leaving one of the breakpoints\n *                 outside a @media query let's you use the grid in IE8.\n *  \n */\n.grid-order`,
+    "(resolution: 2dppx), (-webkit-device-pixel-ratio: 2), (-moz-device-pixel-ratio: 2), (resolution: 192dpi)'",
+    `(min-resolution: 192dpi), (-webkit-min-device-pixel-ratio: 2), (min--moz-device-pixel-ratio: 2)'`,
+    `(max-width: 350px), (min-width: 500px) and (max-width: 600px)'`,
+    `(min-width: 500px), tv and (min-width: 700px) and (color)'`,
+    `(min-width: 31.25em), tv and (min-width: 43.75em) and (color)'`,
+    `(height: 500px)'`,
+    "(lion: 500px)'",
+    "(orientation: portrait)'",
+    "(height: 31.25em)'",
+    "(lion: 31.25em)'",
+    "not screen and (min-width: 500px)'",
+  ]);
+
+  const mqlErrorIndexMap = new Map<string, number[]>(errorsMap);
+
+  for (const testQueryList of testQueryLists) {
+    const mql = parseMediaQueryList(testQueryList);
+    expect(isParserError(mql)).toBe(shouldBeErrorSet.has(testQueryList));
+    if (!isParserError(mql)) {
+      const neverQueries: number[] = [];
+      for (const [i, mq] of mql.mediaQueries.entries()) {
+        if (mq.prefix === "not" && mq.mediaType === undefined && mq.mediaCondition === undefined) {
+          neverQueries.push(i);
+        }
+      }
+
+      expect(neverQueries).toEqual(mqlErrorIndexMap.get(testQueryList) ?? []);
+    }
+  }
+
+  // expect(parseMediaQuery());
+});
